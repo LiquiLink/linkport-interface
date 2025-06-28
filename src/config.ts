eae1243f2e96e673197c5bf200b8090cc057baaf
lack <lackleeweb3@gmail.com>
2025-06-28 18:20:05 +0800

    new deploy

--- src/config.ts
+++ src/config.ts
@@ -15,8 +15,8 @@ export const config = createConfig({
 });
 
 export const linkPorts = {
-    [sepolia.id]: "0x110B273c4DB995188602492599a583B9eAfD74d0",
-    [bscTestnet.id]: "0x24F81DA0aBBD2a88605E4B140880647F26178744",
+    [sepolia.id]: "0xD03BAe9d0367ad9241243408D1137AfC92F2efe6",
+    [bscTestnet.id]: "0xDC64753A100619a00aC950dA011c9eAB9B5aC870",
 } 
 
 export const chainSelector = {
@@ -34,7 +34,7 @@ export const poolList = [
         apy: '8.3%',
         isNative: false,
         address: "0x391e62e754caa820b606703d1920c34a35792dd6",
-        pool: "0x748Ea283F44FbF2da72Abace4575CD8F57D706dd",
+        pool: "0x74B4A8Da407ba782B60a0B8d46f4C461457AA33a",
         volume24h: '$89K',
         tokens: ['LINK']
     },
@@ -45,7 +45,7 @@ export const poolList = [
         apy: '8.3%',
         isNative: false,
         address: "0xa28C606a33AF8175F3bBf71d74796aDa360f4C49",
-        pool: "0x33e0Eee584352f61490F91951B162E38d0a6EeD7",
+        pool: "0xC5519895693F99c2Dad42d2553613FD171A5eE99",
         volume24h: '$89K',
         tokens: ['USDT']
     },
@@ -56,7 +56,7 @@ export const poolList = [
         apy: '8.3%',
         isNative: false,
         address: "0xDC64753A100619a00aC950dA011c9eAB9B5aC870",
-        pool: "0x88322f612dc9e0ed043f766de227bd7ada86d78e",
+        pool: "0xEBF6D13F1F1B25bf994328cB7F8809e657b8DBe8",
         volume24h: '$89K',
         tokens: ['BNB']
     },
@@ -67,7 +67,7 @@ export const poolList = [
         apy: '8.3%',
         isNative: true,
         address: "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14",
-        pool: "0x3812A2D9925bA5FD8915d8B0b8cc6A00fe0ed808",
+        pool: "0xa551b8dBbFE5f82159C3e4ea16D98b790B522038",
         volume24h: '$89K',
         tokens: ['USDT']
     },
@@ -78,7 +78,7 @@ export const poolList = [
         apy: '8.3%',
         isNative: false,
         address: "0xf11935eb67fe7c505e93ed7751f8c59fc3199121",
-        pool: "0x70093330B51754edC72053CbA3bDF9208fC85547",
+        pool: "0x8E40A704Bdfa53B621D6A2F30B8F13dCaA8b8196",
         volume24h: '$89K',
         tokens: ['LINK']
     },
@@ -89,7 +89,7 @@ export const poolList = [
         apy: '8.3%',
         isNative: false,
         address: "0x5016F623414b344a5C26ffDa4e61956c9a41Ca1e",
-        pool: "0x8dd2DeDd22C63667b82575E9E59DC43612CE1758",
+        pool: "0x8a227Fa6368A2745358F85ccdEA105048119e9fD",
         volume24h: '$89K',
         tokens: ['USDT']
     },
@@ -100,7 +100,7 @@ export const poolList = [
         apy: '8.3%',
         isNative: true,
         address: "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd",
-        pool: "0x6a2C375d743382eB7ee79A8cEBA8aB8dA3e9d99a",
+        pool: "0xD5dA88e7aa61291D0f03e2A73eE58C46A49ae605",
         volume24h: '$89K',
         tokens: ['BNB']
     },
